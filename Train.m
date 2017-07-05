f78385e8ce00f625fe30ee97f6535a2bc40931fe
joey <zy0422_lei@hotmail.com>
2017-07-05 22:29:18 +0800

    提交模型的保存和读取
    提交模型的保存和读取

--- Train.m
+++ Train.m
@@ -4,9 +4,11 @@ feature1 = importdata('feature1.txt');
 feature2 = importdata('feature2.txt');
 [no, class, filenames] = textread('Char_Index.txt', '%d	%d	%s', 1000, 'headerlines', 1);
 
-x = [feature1(1:1000, 2:140), feature2(1:1000, 2:73)];
+x = feature1(1:1000, 2:140);
 
 model = svmtrain(class, x, '-c 1 -g 0.07');
-[predict_label, accuracy, dec_values] = svmpredict(class, x, model);
+save('model.dat', 'model');
+model1=load('model.dat', '-mat');
+[predict_label, accuracy, dec_values] = svmpredict(class, x, model1.model);
 
 toc;
